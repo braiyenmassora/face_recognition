8ced8b31e174f426b3ab2d89f9b52dfbcea2199f
Braiyen Massora <braiyenmassora@gmail.com>
2020-02-03 13:25:59 +0700

    Create indentifyFaces.py

---
+++ indentifyFaces.py
@@ -0,0 +1,21 @@
+import face_recognition
+from PIL import Image, ImageDraw
+
+image = face_recognition.load_image_file ("/Desktop/ProjectCv2/faceRecognation/dataImage/a.jpg")
+face_locations = face_recognition.face_locations(image)
+face_encodings = face_recognition.face_encodings(image, face_locations)
+
+
+pil_image = Image.fromarray(image)
+
+draw = ImageDraw.Draw(pil_image)
+
+for (top, right, bottom ,left ), face_locations in zip (face_locations, face_encodings):
+    draw.rectangle(((left, top),(right, bottom)), outline=(127,255,0))
+
+
+
+
+
+del draw
+pil_image.show()
